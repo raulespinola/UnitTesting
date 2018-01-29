99fe8b82d094748848daf243b2067012858cb2e5
Raul Espinola <raulespinola@gmail.com>
2018-01-29 15:17:55 -0300

    2nd Commit

--- src/com/junit/chap3/TestDefaultController.java
+++ src/com/junit/chap3/TestDefaultController.java
@@ -23,12 +23,12 @@ public class TestDefaultController {
 	public void testMethod() {
 		//throw new RuntimeException ("implement me");
 	}
-	
+	@Test
 	public void testAddHandler(){
 		RequestHandler handler2= controller.getHandler(request);
 		assertSame(handler2,handler);
 	}
-	
+	@Test
 	public void testProcessRequest(){
 			
 		Response response = controller.processRequest(request);
@@ -37,10 +37,67 @@ public class TestDefaultController {
 		assertEquals(new TestResponse(), response);
 	}
 	
+	@Test
+	public void testProcessRequesAnsweresErrorResponse(){
+		TestRequest request = new TestRequest("testError");
+		TestExceptionHandler handler = new TestExceptionHandler();
+		controller.addHandler(request, handler);
+		Response response = controller.processRequest(request);
+		assertNotNull("Must not return a null reponse", response);
+		assertEquals(ErrorResponse.class, response.getClass());
+	}
+	
+	@Test
+	public void testGetHandlerNotDefined(){
+		TestRequest request = new TestRequest("testNotDefined");
+		try{
+			controller.getHandler(request);
+			fail("An exception shour be raised if the request"
+					+ "handles has not been registered");
+		}
+		catch(RuntimeException expected){
+			assertTrue(true);
+		}
+	}
+	
+	@Test
+	public void TestAddRequestDuplicateName(){
+		TestRequest request = new TestRequest();
+		TestHandler handler = new TestHandler();
+		
+		try{
+			controller.addHandler(request, handler);
+			fail ("An Exception should be raised if the default"
+			+ "TestRequest has already been registered");
+		}
+		catch (RuntimeException expected){
+			assertTrue(true);
+		}
+	}
+	
+	
+	
+	private class TestExceptionHandler implements RequestHandler{
+		public Response process (Request request) throws Exception{
+				throw new Exception ("error processing request");
+		}
+	}
 	
 	private class TestRequest implements Request{
+		private static final String DEFAULT_NAME = "test";
+		private String name;
+		
+		public TestRequest (String name){
+			this.name= name;
+		}
+		
+		public TestRequest(){
+			this(DEFAULT_NAME);
+		}
+		
+		
 		public String getName(){
-			return "Test";
+			return this.name;
 		}
 	}
 	private class TestHandler implements RequestHandler{
